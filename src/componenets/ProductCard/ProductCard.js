5786221c4a98e64af626e9d2a1dd4683afa9e869
IlyaMeli <ilyameli93@gmail.com>
2021-12-26 09:08:01 +0200

    work in progress

--- src/componenets/ProductCard/ProductCard.js
+++ src/componenets/ProductCard/ProductCard.js
@@ -2,18 +2,19 @@ import React from "react";
 import "./productcard.css";
 const ProductCard = ({
   product: { name, description, price, img_URL, discount },
-  handleClick,product
+  handleClick,
+  product,
 }) => {
   return (
     <div className="product-card">
-      <img src={img_URL} alt="picture" />
+      <img src={img_URL} alt={name} />
       <div>{name}</div>
       <div>{description}</div>
       <div className="price-container">
         <span>{price}$</span>
         <span className="discount">{discount}$</span>
       </div>
-      <button className="add-to-cart" onClick={() =>handleClick(product)}>
+      <button className="add-to-cart" onClick={() => handleClick(product)}>
         Add To Card
       </button>
     </div>
